80aec62b3989db4cb454343788c49e125c6d5a82
Guido Santillan <guidosantillan01@gmail.com>
2019-12-22 20:51:25 +0100

    Add post signup request

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const authRoutes = require('./routes/authRoutes');
+
 const app = express();
+
+app.use(authRoutes);
 
 const mongoUri =
   'mongodb+srv://guido:guido@cluster0-xbnyy.mongodb.net/test?retryWrites=true&w=majority';
